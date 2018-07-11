641ebdda79bb8963ac13708e983da634eb3aca10
Dmytro Halimov <dmitrii.halimov@acropolium.com>
2018-07-11 13:22:44 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      Задайте вопрос, на который можно ответить "да" или "нет":
+      <input type="text" v-model="question">
+    </p>
+    <p>{{ answer }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import _ from "lodash";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      question: "",
+      answer: "Пока вы не зададите вопрос, я не могу ответить!"
+    };
+  },
+  watch: {
+    question: function(newQuestion, _oldQuestion) {
+      this.answer = "Ожидаю, когда вы закончите печатать...";
+      this.debounceAnswer();
+    }
+  },
+  created() {
+    this.debounceAnswer = _.debounce(this.getAnswer, 500);
+  },
+  methods: {
+    getAnswer() {
+      if (this.question.indexOf("?") === -1) {
+        this.answer = "Вопросы обычно заканчиваются вопросительным знаком. ;-)";
+
+        return;
+      }
+      this.answer = "Думаю...";
+      const vm = this;
+      axios
+        .get("https://yesno.wtf/api")
+        .then(function(response) {
+          vm.answer = _.capitalize(response.data.answer);
+        })
+        .catch(function(error) {
+          vm.answer = "Ошибка, Не могу связаться с API. " + error;
+        });
+    }
   }
 };
 </script>
